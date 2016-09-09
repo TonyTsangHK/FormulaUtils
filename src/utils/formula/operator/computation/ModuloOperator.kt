66f7c2e934482e4100ed7b07fc8136e18f0672f7
Tony Tsang <klsstsang@yahoo.com>
2016-09-09 14:40:27 +0800

    Add ModuloOperator
    -  Modulo expression is mod not %, this is required to distinguish between percentage and modulo operators

---
+++ src/utils/formula/operator/computation/ModuloOperator.kt
@@ -0,0 +1,40 @@
+package utils.formula.operator.computation
+
+import utils.formula.component.FormulaComponent
+import utils.formula.operator.ComputationOperator
+import utils.formula.operator.Operator
+import java.math.BigDecimal
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Tony Tsang
+ * Date: 2016-09-09
+ * Time: 12:02
+ */
+object ModuloOperator: ComputationOperator {
+    override val componentType: FormulaComponent.Type
+        get() = FormulaComponent.Type.OPERATOR
+    override val formulaComponentExpression: String
+        get() = EXPRESSION
+    override val precedence: Operator.Precedence
+        get() = Operator.Precedence.MIDDLE
+
+    // Can't use %, since it is occupied by Percentage operation
+    val EXPRESSION = "mod"
+
+    override fun compute(leftOperand: BigDecimal, rightOperand: BigDecimal): BigDecimal {
+        return leftOperand.remainder(rightOperand)
+    }
+
+    override fun isOperatorExpression(expression: String): Boolean {
+        return expression == EXPRESSION
+    }
+
+    override fun isStartWithOperatorExpression(expression: String, startIndex: Int): Boolean {
+        return expression.startsWith(EXPRESSION, startIndex)
+    }
+
+    override fun toString(): String {
+        return EXPRESSION
+    }
+}
